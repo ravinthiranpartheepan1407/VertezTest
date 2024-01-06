4b3a729a83f119d29f7e6796e1932a8a31b44b7d
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2024-01-06 14:15:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using vertezml;
+
+class VertezTest
+{
+    static void Main()
+    {   
+        // Calculate Mean
+        List<double> values = new List<double> { 10, 20, 30, 40, 50 };
+        double calc = vertezml.MeanOp.CalculateMean(values);
+        Console.WriteLine(calc);
+    }
+}
